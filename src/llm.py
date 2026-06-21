4888ce3e3034339f0d8ab2ff96766664c0c76ea1
rafsun-jany-rafy <rafsun.rme18@gmail.com>
2026-06-22 05:38:54 +0600

    Updating requirements and llm.py for fastapi

--- src/llm.py
+++ src/llm.py
@@ -1,28 +1,10 @@
-import ollama
+import os
+from groq import Groq
+from dotenv import load_dotenv
 
-#without stream
-# def generate_answer(query, context_chunks):
-#     # join the chunks into one string to provide context
-#     context_text = "\n\n".join(context_chunks)
-    
-#     # The system prompt tells the AI how to behave
-#     system_prompt = (
-#         "You are a helpful assistant. Use the provided context to answer the question. "
-#         "If the answer is not in the context, say you don't know. Keep it professional. "
-#     )
-    
-#     # The user prompt contains the context and the actual question
-#     user_prompt = f"Context: \n{context_text} \n\nQuestion: {query}"
-    
-#     response = ollama.chat(
-#         model = 'llama3.2',
-#         messages = [
-#             {'role': 'system', 'content': system_prompt},
-#             {'role': 'user', 'content': user_prompt}
-#         ]
-#     )
-    
-#     return response['message']['content']
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def generate_answer_stream(query, context_chunks):
         # join the chunks into one string to provide context
@@ -38,8 +20,8 @@ def generate_answer_stream(query, context_chunks):
         user_prompt = f"Context: \n{context_text} \n\nQuestion: {query}"
         
         # using stream = True to get a generator
-        stream = ollama.chat(
-            model = 'llama3.2',
+        stream = client.chat.completions.create(
+            model = 'llama-3.3-70b-versatile',
             messages = [
                 {'role': 'system', 'content': system_prompt},
                 {'role': 'user', 'content': user_prompt}
